7f85f23aae92796170dedaa0c18da78f4531ac86
MilanAljetic <aljetic23@gmail.com>
2020-04-24 00:13:39 +0200

    blog-2.0

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.urls import include
 from django.contrib.auth import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
